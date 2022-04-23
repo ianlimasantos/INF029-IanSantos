a92c8b7a0ef5140e7feecbc22cbfacc9e2151dca
ianlimasantos <99565712+ianlimasantos@users.noreply.github.com>
2022-04-23 22:16:37 +0000

    q6 certa

--- q6jesus/q6.c
+++ q6jesus/q6.c
@@ -5,7 +5,7 @@ int q6(int numerobase, int numerobusca);
 
 int main (){ 
   //q6(1434, 4);
-  q6(4415446344, 44);
+  q6(12111011, 11);
 }
 
 
@@ -17,58 +17,56 @@ int q6(int numerobase, int numerobusca){
     //copia2 = copia1;
    
     if (numerobase == 0 && numerobusca == 0)
-        qtdOcorrencias ++;
+      qtdOcorrencias ++;
        
     while (numerobase > 0){
       //      numerobusca = copia2;
         restonumerobase = numerobase % 10;
         restonumerobusca = numerobusca % 10;
-                printf ("######\n");
-                printf (" O resto numero base vale %d\n", restonumerobase);
-                printf (" O resto numero busca vale %d\n", restonumerobusca);
-                printf ("######\n");
                 
         if (restonumerobase == restonumerobusca){
           contador = 1;
           while (restonumerobase == restonumerobusca && numerobase>0 && contador>0){
-              numerobase = numerobase / 10;
-              numerobusca = numerobusca / 10;
-              restonumerobase = numerobase % 10;
-              restonumerobusca = numerobusca % 10;
-                    printf (" O numero base vale %d\n", numerobase);
-                    printf (" O numero busca vale %d\n", numerobusca);
-                    printf (" O resto numero base vale %d\n", restonumerobase);
-                    printf (" O resto numero busca vale %d\n", restonumerobusca);
+              numerobase = numerobase / 10; //202222 //2022
+              numerobusca = numerobusca / 10; //2; //
+              restonumerobase = numerobase % 10; //2
+              restonumerobusca = numerobusca % 10; //2 
+                  printf (" O numero base vale %d\n", numerobase);
+                  printf (" O numero busca vale %d\n", numerobusca);
+                  printf (" O resto numero base vale %d\n", restonumerobase);
+                  printf (" O resto numero busca vale %d\n", restonumerobusca);
                    
                    
-                if (numerobusca<10){
-                   // restonumerobusca = numerobusca;
-                   // printf (" - O resto numero busca vale %d\n", restonumerobusca);
+                if (numerobusca<10 && numerobusca == restonumerobase ){
+                  restonumerobusca = numerobusca;
+                  printf (" - O resto numero busca vale %d\n", restonumerobusca);
                     contador = -1;
                    
                 }
               if (contador<0){
-                 qtdOcorrencias ++;
-                        printf (" A quantidade de ocorrencias foi %d\n", qtdOcorrencias);
-                        numerobase = numerobase / 10;
-                        printf ("AAAAAAAAAAAAAAAAA\n");
-                        printf (" O numero base vale %d\n", numerobase);
-                        printf ("AAAAAAAAAAAAAAAAA\n");
-                    }
+                qtdOcorrencias ++;
+                printf (" A quantidade de ocorrencias foi %d\n", qtdOcorrencias);
+                numerobase = numerobase / 10;//20222
+                numerobusca = copia1;
+                        
+                printf ("AAAAAAAAAAAAAAAAA\n");
+                printf (" O numero base vale %d\n", numerobase);
+                printf ("AAAAAAAAAAAAAAAAA\n");
+              }
                
             }
-        }
-        
-         if (qtdOcorrencias>i)  {
-          i++;
-          numerobusca = copia1;
-        }
+          }
         else{
-            numerobase = numerobase / 10;
-            numerobusca = copia1;   
+            numerobase = numerobase / 10; //202222 
+            numerobusca = copia1;   //22
+
+            printf ("ELSE PRINTADO BBS\n");
+            printf (" O numero base vale %d\n", numerobase);
+            printf (" O numero busca vale %d\n", numerobusca);
+            printf ("ELSE PRINTADO BBS\n");
         }
     }
-
+printf (" A quantidade de ocorrencias foi %d\n", qtdOcorrencias);
     return qtdOcorrencias;
 }
  
